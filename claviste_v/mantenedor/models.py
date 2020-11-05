5146b2bfca910768a3826f9db192e20ff2c853e4
ifigueroator <ifigueroatoro2@gmail.com>
2020-11-05 10:07:19 -0600

    Se modifica modelo para listar informacion de las tablas

--- claviste_v/mantenedor/models.py
+++ claviste_v/mantenedor/models.py
@@ -7,11 +7,17 @@ class regitro_cliente(models.Model):
     email=models.EmailField()
     telefono=models.CharField(max_length=9)
 
+    def __str__(self):
+        return '%s  %s  %s  %s  %s'%(self.nombre,self.apellidoP,self.apellidoM,self.email,self.telefono)
+
 class nuevos_plan(models.Model):
     nombre_plan=models.CharField(max_length=30)
     precio_plan=models.IntegerField()
     fecha_inicio=models.DateField()
     fecha_termino=models.DateField()
+
+    def __str__(self):
+        return '%s  $%s Inicio:%s  Termino:%s'%(self.nombre_plan,self.precio_plan,self.fecha_inicio,self.fecha_termino)
     
 
     
