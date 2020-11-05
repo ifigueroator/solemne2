25700d2281c00dd1f9879f4abe96ce5b7497b502
ifigueroator <ifigueroatoro2@gmail.com>
2020-11-05 11:38:05 -0600

    Se crean titulos en la columnas de las tablas

--- claviste_v/mantenedor/admin.py
+++ claviste_v/mantenedor/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from mantenedor.models import regitro_cliente,nuevos_plan
 
-admin.site.register(regitro_cliente)
-admin.site.register(nuevos_plan)
+class nuevoCliente(admin.ModelAdmin):
+    list_display=("nombre","apellidoP","apellidoM","email","telefono")
+
+class registro(admin.ModelAdmin):
+    list_display=("nombre_plan","precio_plan","fecha_inicio","fecha_termino")
+
+admin.site.register(regitro_cliente,nuevoCliente)
+admin.site.register(nuevos_plan,registro)
